227de84f385aaa5278de0799ff016a539be342dc
Shengxi-cai <caisxl@163.com>
2021-08-12 13:07:22 +0800

    8月12号

---
+++ test_20210621/test_20210621/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include<stdio.h>
+
+void print(int(*p)[5], int x, int y)
+{
+	int i = 0;
+	for (i = 0; i < x; i++)
+	{
+		int j = 0;
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", *(*(p + i) + j));
+			printf("%d ", p[i][j]);//arr[i]=*(p+i)=p[i]
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int arr[3][5] = { { 1, 2, 3, 4, 5 }, { 2, 3, 4, 5, 6 }, { 3, 4, 5, 6, 7 } };
+	print(arr, 3, 5);
+	return 0;
+}
